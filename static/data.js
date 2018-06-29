03946a5940a99ff0f77205bf3a6167d5d8efb76e
Daichi Teruya <tmarusa83@gmail.com>
2018-06-29 17:40:46 +0900

    add title-en in prizes

--- static/data.js
+++ static/data.js
@@ -89,40 +89,51 @@ var data = {
   [
     {
       title: '優秀リコンフィギャラブルシステム講演賞',
+      etitle: 'IEICE RECONF, Outstanding Reconfigualable System Presentation Award',
       year: 2018
     }, {
       title: "電子情報通信学会論文誌D 学生秀逸論文",
+      etitle: "IEICE Transactions D (Japansese), Students Excellent Paper",
       year: 2017
     }, {
       title: "2014年度沖縄工業高等専門学校メディア情報工学科表彰 優秀学生表彰2件・最優秀卒業研究賞",
+      etitle: "Okinawa-NCT Media Information Department Commendation, Excellent student Prize x2, Best Graduation Research Prize",
       year: 2015
     }, {
       title: "2014年トレンドマイクロプログラミングコンテスト 国内7位入賞",
+      etitle: "Trendmicro Programming Contest, A Prize Winner (7th)",
       year: 2014
     }, {
       title: "IT津梁まつり2013 プログラム部門 第一位",
+      etitle: "IT Shinryo Matsuri, Prigramming Part, 1st Prize",
       year: 2013
     }, {
       title: "第24回全国高等専門学校プログラミングコンテスト 自由部門 特別賞",
+      etitle: "24th NIT Programming Contst, Free Part, Special Prize",
       year: 2013,
       url: { url: "http://www.procon.gr.jp/?p=73190", name: "Procon 24th Results" }
     }, {
       title: "平成24年度高専機構理事長特別賞",
+      etitle: "National Institutes of Technology President Special Prize",
       year: 2013,
       url: { url: "http://www.kosen-k.go.jp/news/news20130520.html", name: "NIT Information" }
     }, {
       title: "情報処理学会若手奨励賞",
+      etitle: "IPSJ Excellent Student Award",
       year: 2012
     }, {
       title: "パソコン甲子園2012 モバイル部門 グランプリ",
+      etitle: "Pasocon Koshien 2012, Mibile Part, Grand Prize",
       year: 2012,
       url: { url: "http://web-ext.u-aizu.ac.jp/pc-concours/2012hon/13/13_kekka.html", name: "PCK 2012 Results" }
     }, {
       title: "第23回全国高等専門学校プログラミングコンテスト 自由部門 特別賞・イグジットチューンズ賞",
+      etitle: "23th NIT Programming Contst, Free Part, Special Prize and Exit Tunes Prize",
       year: 2012,
       url: { url: "http://www.procon.gr.jp/?p=73908", name: "Procon 23th Results" }
     }, {
       title: "EPOCH@まつやま2011 サイボウズ社長賞",
+      etitle: "EPOCH@Matsuyama 2011, Cybozu Inc. President's Prize",
       year: 2011
     }
   ],
@@ -130,39 +141,47 @@ var data = {
   [
     {
       title: "第7回相磯秀夫杯デザインコンテストTRAXデザインコンペティション",
+      etitle: "7th Aiso Hideo Cup Design Contest TRAX Design Competition",
       project: "NokodAI",
       year: 2016
     }, {
       title: "Git Challenge 2015",
+      etitle: "Git Challenge 2015",
       year: 2015,
       url: { url: "http://maruuusa83.hateblo.jp/entry/2015/11/15/223917", name: "Blog" }
     }, {
       title: "Scrap Challenge 2015",
+      etitle: "Scrap Challenge 2015",
       year: 2015,
       url: { url: "http://maruuusa83.hateblo.jp/entry/2015/08/29/000000", name: "Blog" }
     }, {
       title: "2014年トレンドマイクロプログラミングコンテスト",
+      etitle: "Trendmicro Programming Contest",
       year: 2014,
       prize: '<span class="important">国内7位入賞</span>',
     }, {
       title: "第24回全国高等専門学校プログラミングコンテスト自由部門本選",
+      etitle: "24th NIT Programming Contest, Free Part",
       year: 2013,
       project: "Anyware",
       url: { url: "http://www.procon.gr.jp/?page_id=59711", name: "Procon 24th Web Site" },
       prize: '<span class="important">特別賞 (3位相当)</span>',
     }, {
       title: "第23回全国高等専門学校プログラミングコンテスト自由部門本選",
+      etitle: "23th NIT Programming Contest, Free Part",
       year: 2012,
       project: "かしこん！！",
       url: { url: "http://www.procon.gr.jp/?page_id=59691", name: "Procon 23rd Web Site"},
       prize: '<span class="important">特別賞 (3位相当)</span> <span class="important">イグジットチューンズ賞 (企業特別賞)</span>',
     }, {
       title: "パソコン甲子園2012 プログラミング部門本選",
+      etitle: "Pasocon Koshien 2012 Programming Part",
       year: 2012,
       team: "ついてきてる？",
       url: { url: "http://web-ext.u-aizu.ac.jp/pc-concours/2012hon/13/13_team.html", name: "PCK2012 Web Site"},
     }, {
       title: "パソコン甲子園2012 モバイル部門本選",
+      etitle: "Pasocon Koshien 2012 Mobile Part",
       year: 2012,
       team: "フレッシュクオリティ",
       project: "RunRunRun",
@@ -170,19 +189,23 @@ var data = {
       prize: '<span class="important">グランプリ</span>',
     }, {
       title: "第22回全国高等専門学校プログラミングコンテスト競技部門本選",
+      etitle: "22th NIT Programming Contest, Sports Programming Part",
       year: 2011,
       project: "Mado Magi System",
       url: { url: "http://www.procon.gr.jp/?page_id=59681", name: "Procon 22nd web site" },
     }, {
       title: "EPOCH@まつやま2011 本選",
+      etitle: "EPOCH@Matsuyama 2011",
       year: 2011,
       team: "五年保障",
       prize: '<span class="important">サイボウズ社長賞</span>',
     }, {
       title: "日本情報オリンピック 本選",
+      etitle: "Japanese Olympiad of Information",
       year: 2011
     }, {
       title: "EPOCH@まつやま2010 本選",
+      etitle: "EPOCH@Matsuyama 2010",
       year: 2010,
       team: "三年保障"
     },
@@ -191,6 +214,7 @@ var data = {
   [
     {
       title: "IT津梁まつり2013 プログラム部門",
+      etitle: "IT Shinryo Matsuri 2013 Programming Part",
       year: 2013,
       prize: '<span class="important">第一位</span>',
     }
@@ -199,14 +223,17 @@ var data = {
   [
     {
       title: "平成29年度未踏IT人材発見・育成プロジェクト 採択",
+      etitle: "2017 MITOU Project",
       year: 2017,
       text: '「CPU＋FPGAプラットフォームのためのRubyベースの開発環境」<span class="important">スーパークリエイター認定</span>',
       url: { url: "https://www.ipa.go.jp/jinzai/mitou/2017/seika.html", name: "IPA web site"}
     }, {
       title: "ITfrogs 4期生",
+      etitle: "ITfrogs 4th graduating team",
       year: 2012
     }, {
       title: "日本情報オリンピック 夏季合宿",
+      etitle: "IPSJ Summer Camp",
       year: 2011
     }
   ],
